eba161e33fd6941a96554c4656f080dac4502a28
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-22 09:29:05 +0300

    fix(docs): added documentation

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
+-- Script that creates table users if i doesn't exist
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255)
+        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        name VARCHAR(255)
 );
